750db6cd9648e902c93a8001a75528e899ae8a37
Lankaster <info@lank.me>
2012-11-01 21:12:50 +0100

    upd

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ define( '_NE', 1 );
 include 'modxclass.php';
 $wwwpath=(empty($_GET['q'])) ? '' : htmlspecialchars($_GET['q']); // Active Link	
 
-$content = DB::getInstance();
+//$content = DB::getInstance();
+$content = new DB ();
 $htmlout=$content->getPageFromAlias($wwwpath);
 
-printf (preg_replace( "/\[\[time\]\]/", "2 queries, phptime  s".$time, $htmlout ));
+printf ($htmlout );
 ?>
--- modxclass.php
+++ modxclass.php
@@ -7,58 +7,38 @@ class DB {
 	private $DB_template;
 	private $DB_content;
 	//db configuration 
-	private $hostname = "localhost";
+	private $hostname = "127.0.0.1";
 	private $username = "max";
 	private $password = "maxmax";
 	private $dbName = "max";
 	private $userstable = "modx_site_content";
 	private $templatetable = "modx_site_templates_fast";
 
-	
-	private function __construct()
-	{
-	}
 	public final function __clone()
 	{
 		throw new BadMethodCallException("Clone is not allowed");
 	}
 	/**
-	 * getInstance
-	 *
-	 * @static
-	 * @access public
-	 * @return object DB instance
-	 */
-	public static function getInstance()
-	{
-		if (!(self::$dbobj instanceof DB)) {
-			self::$dbobj = new DB;
-		}
-		return self::$dbobj;
-	}
-	/**
-	 * __BaseConnect()
+	 * BaseConnect()
 	 * Set or get connection to base
 	 * @static
 	 * @access private
 	 * @return $DB_dbh
 	 */
-	private function __BaseConnect() {
-		if (!(self::$DB_dbh instanceof PDO)) {
-		self::$DB_dbh = new PDO("mysql:host='".$hostname."';dbname='".$dbName."'",$username,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-		}
-		return self::$DB_dbh;
+	private function BaseConnect() {
+		$this->DB_dbh = new PDO('mysql:host='.$this->hostname.';dbname='.$this->dbName ,$this->username,$this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		return $this->DB_dbh;
 	}
 	/**
-	 * __BaseClose()
+	 * BaseClose()
 	 * Close connection
 	 * @static
 	 * @access private
 	 * @return null
 	 */
-	private function __BaseClose() {
+	private function BaseClose() {
 		//Close connection
-		self::$DB_dbh = NULL;
+		$this->DB_dbh = NULL;
 	}
 	
 	/**
@@ -69,17 +49,19 @@ class DB {
 	 * @access public
 	 * @return Get content from uri
 	 */
-	public static function getPageFromAlias($uri) 
+	public function getPageFromAlias($uri) 
 	{
 		
-		$conn=self::__BaseConnect();
+		//$conn=self::BaseConnect();
+		$conn=$this->BaseConnect();
+		
 		// Get content
-		$sth = $conn->prepare("SELECT id as 'id', pagetitle as 'title' , template as 'template', content as 'content' FROM ".$userstable." WHERE uri= '".$uri."' LIMIT 0 , 1");
+		$sth = $conn->prepare("SELECT id as 'id', pagetitle as 'title' , template as 'template', content as 'content' FROM ".$this->userstable." WHERE uri= '".$uri."' LIMIT 0 , 1");
 		$sth->execute();
 		$content = $sth->fetch(PDO::FETCH_ASSOC);
 		$sth->closeCursor();
 		
-		$sth = $conn->prepare("SELECT content as 'content' FROM ".$templatetable." WHERE id= '6' LIMIT 0 , 1");
+		$sth = $conn->prepare("SELECT content as 'content' FROM ".$this->templatetable." WHERE id= '6' LIMIT 0 , 1");
 		$sth->execute();
 		$template= $sth->fetch(PDO::FETCH_ASSOC);
 		$sth->closeCursor();
